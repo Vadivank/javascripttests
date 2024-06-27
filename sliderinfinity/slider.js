21bb0c936ac0fee5be98e491d3372192c5d9d882
Vadim V. Ivankov <ivankov_vv@risde.ru>
2024-06-27 14:40:23 +0300

    fix: minor modified: *animation in js code, *styles

--- sliderinfinity/slider.js
+++ sliderinfinity/slider.js
@@ -40,10 +40,9 @@ const nextImageGenerate = () => {
     img.alt = '';
     img.src = './images/' + images[nextImage];
     sliderPlace.append(img);
-    console.log(images[nextImage], nextImage);
 };
 
-const prevImageGenerate = () => {
+const prevImageGenerate = (w = false) => {
     let prevImage = activeImage - 1;
     if (prevImage < 0) {
         prevImage = images.length - 1;
@@ -51,18 +50,75 @@ const prevImageGenerate = () => {
     const img = document.createElement('img');
     img.alt = '';
     img.src = './images/' + images[prevImage];
+    if (w) {
+        img.style.width = 0;
+    }
     sliderPlace.prepend(img);
 };
 
 const nextSlide = () => {
+    if (!flag) {
+        return;
+    }
+    flag = !flag;
+
     activeImage++;
     if (activeImage >= images.length) {
         activeImage = 0;
     }
+    // document.querySelector('.slider-line img').remove();
     nextImageGenerate();
+    animate({
+        duration: 1000,
+        draw: function (progress) {
+            document.querySelector('.slider-line img').style.width = (widthOffset * (1 - progress)) + 'px';
+        },
+        removeElement: document.querySelector('.slider-line img')
+    });
 }
 
+const prevSlide = () => {
+    if (!flag) {
+        return;
+    }
+    flag = !flag;
+
+    activeImage--;
+    if (activeImage < 0) {
+        activeImage = images.length - 1;
+    }
+    // document.querySelector('.slider-line img:last-child').remove();
+    prevImageGenerate(true);
+    animate({
+        duration: 1000,
+        draw: function (progress) {
+            document.querySelector('.slider-line img').style.width = (widthOffset * progress) + 'px';
+        },
+        removeElement: document.querySelector('.slider-line img:last-child')
+    });
+};
 
 initSlider();
 
 document.querySelector('.next-button').addEventListener('click', nextSlide);
+document.querySelector('.prev-button').addEventListener('click', prevSlide);
+
+const animate = ({ duration, draw, removeElement }) => {
+    const start = performance.now();
+
+    requestAnimationFrame(function animate(time) {
+        let step = (time - start) / duration;
+        if (step > 1) {
+            step = 1;
+        }
+
+        draw(step);
+
+        if (step < 1) {
+            requestAnimationFrame(animate);
+        } else {
+            removeElement.remove();
+            flag = true;
+        }
+    });
+};
